30d70e8ff2dbfc73b173b76f014970b3ddbde9be
michdev7 <michele.bev@outlook.com>
2023-02-11 17:00:43 +0100

    Initial commit

---
+++ app/src/main/java/com/diegusmich/intouch/helpers/ActivityHelper.kt
@@ -0,0 +1,22 @@
+package com.diegusmich.intouch.helpers
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import com.google.android.material.elevation.SurfaceColors
+
+class ActivityHelper {
+
+    companion object {
+
+        fun setFullScreen(activity: AppCompatActivity){
+
+            //Mostra a schermo intero l'activity
+            WindowCompat.setDecorFitsSystemWindows(activity.window, false)
+
+            //Rende trasparente la status bar e la navigation bar
+            activity.window.statusBarColor = Color.TRANSPARENT // Set color of system statusBar same as ActionBar
+            activity.window.navigationBarColor =  SurfaceColors.SURFACE_2.getColor(activity)
+        }
+    }
+}
